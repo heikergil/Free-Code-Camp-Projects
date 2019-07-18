2df94aafab197ddd0a716afa2a7daf22f28879a8
Heiker Gil <gilheyker@gmail.com>
2019-07-18 06:21:40 -0500

    fixed #editor, .header and textarea positioning

--- REACT/markdown-previewer/markdownpreviwer/src/App.js
+++ REACT/markdown-previewer/markdownpreviwer/src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+// This inserts the module marked into the current scope, 
+// containing all the exports from the module
 import * as marked from 'marked';
-
+// Get reference, this will change the markdown is rendered from a html like to a github like markdown
 const renderer = new marked.Renderer();
-
+// if true, add <br> on a single line break (copies GitHub). Requires gfm be true.
 marked.setOptions({
   breaks: true,
 });
-
+// main statefull component or container component
 class App extends Component {
   constructor(props) {
     super(props)
+    // state is any data your application needs to know about.
     this.state = {
             input:""
     }
+    // methods bindings
     this.onInputChange = this.onInputChange.bind(this);
   }
+  // when the value of target change run this function
   onInputChange = e => {
     this.setState({ input: e.target.value})
-    console.log(this.state.input);
   }
     
   render() {
@@ -34,12 +38,14 @@ class App extends Component {
   }
   }
 
+// stateless component
   const Editor = (props) => {
     return (
       <div id="editor">
         <div className="header"></div>
-      <textarea 
-        value={props.markdown}
+      <textarea
+      // since 'textarea' cannot comunicate directly with 'app' element, 'onchange' is going to send a signal
+      // to the onchange props passed to the Editor element on render everytime it changes. 
         onChange={props.onChange}
         type="text"/>
         </div>
